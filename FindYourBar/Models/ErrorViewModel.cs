d2d9f6482530ecef7873a2c37f1264aa9e4b58c1
VincentLevel <Vincentlevel21@gmail.com>
2018-10-18 16:51:27 +0200

    FIsrt commit

---
+++ FindYourBar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FindYourBar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
